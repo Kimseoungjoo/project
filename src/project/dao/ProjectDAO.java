9b47d5cfcc2a9af1af7de044e3a6f83b28f46c03
Universe <tmdwnchlrh3@gmail.com>
2021-10-01 18:13:13 +0900

    Driver load

--- src/project/dao/ProjectDAO.java
+++ src/project/dao/ProjectDAO.java
@@ -1,5 +1,12 @@
 package project.dao;
 
 public class ProjectDAO {
-
+ 
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+ }
 }
