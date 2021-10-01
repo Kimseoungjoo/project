9dd322b6a9e699a7a62453279bb830e6fc066c29
park <parkgod97@naver.com>
2021-10-01 18:13:10 +0900

    드라이버 로드

--- src/project/dao/ProjectDAO.java
+++ src/project/dao/ProjectDAO.java
@@ -1,5 +1,14 @@
 package project.dao;
 
 public class ProjectDAO {
-
+	
+	
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
