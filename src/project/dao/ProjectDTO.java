8c9e93259f9a4dedb27669c91bf1c82ead753d5f
Universe <tmdwnchlrh3@gmail.com>
2021-10-05 10:22:48 +0900

    lombok 소스

--- src/project/dao/ProjectDTO.java
+++ src/project/dao/ProjectDTO.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjectDTO {
 	private int classNo;
 	private int classLevel;
