fa60cb5ec009fcf6f48862c38dda8c7e86eca585
Universe <tmdwnchlrh3@gmail.com>
2021-10-05 10:33:18 +0900

    인사~

--- src/learning/Oct_5.java
+++ src/learning/Oct_5.java
@@ -5,7 +5,7 @@ public class Oct_5 {
 public static void main(String[] args) {
 		
 	System.out.println("안녕하세요");
-	
+	System.out.println("안녕하세요~");
 	
 	
 	}
--- src/practice/Practice_yunyewon.java
+++ src/practice/Practice_yunyewon.java
@@ -5,6 +5,7 @@ public class Practice_yunyewon {
 	public static void main(String[] args) {
 
 		System.out.println("안녕하세요");
+		System.out.println("안녕하세요~");
 	}
 
 }
