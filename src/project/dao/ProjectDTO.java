0a01100c7988d0a50c3fcef28a7439ba7c0778c1
park <parkgod97@naver.com>
2021-10-05 10:07:03 +0900

    prject DTO 견본

---
+++ src/project/dao/ProjectDTO.java
@@ -0,0 +1,18 @@
+package project.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class ProjectDTO {
+	private int classNo;
+	private int classLevel;
+	private int classGrade;
+	private String teacher;
+	
+	
+}
