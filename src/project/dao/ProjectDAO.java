1d171baa7cb4c61ea231afda50e3053f8a566978
Universe <tmdwnchlrh3@gmail.com>
2021-10-05 10:04:23 +0900

    getConnection

--- src/project/dao/ProjectDAO.java
+++ src/project/dao/ProjectDAO.java
@@ -1,13 +1,29 @@
 package project.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ProjectDAO {
 
- 
 	static {
 		try {
 			Class.forName("oracle.jdbc.OracleDriver");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
- }
+	}
+	// 연결 소스
+	public static Connection getConnection() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "c##scott";
+		String password = "tiger";
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url,user,password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
 }
